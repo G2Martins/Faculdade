bda8337357cd0f42885029cd407982c49aa8440a
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-06-27 22:13:03 -0300

    Create p1_1.c

---
+++ 1° Semestre/Práticas 4/p1_1.c
@@ -0,0 +1,37 @@
+//Faça um programa em C que preencha uma matriz com 10 números, depois leia um número e localize a posição dele na matriz.
+
+#include <stdio.h>
+
+int main() {
+//         Posição =   0   1  2  3  4  5  6  7  8  9  
+    int numeros[10] = {10, 5, 2, 1, 4, 7, 6, 9, 3, 0};
+
+/* numeros[0] = 10;
+    numeros[1] = 5;
+    numeros[2] = 2;
+    numeros[3] = 1;
+    numeros[4] = 4;
+    numeros[5] = 7;
+    numeros[6] = 6;
+    numeros[7] = 9;
+    numeros[8] = 3;
+    numeros[9] = 0;
+*/  
+
+    int numero = 0;
+
+    printf("Digite um numero inteiro:\n");
+    scanf("%i", &numero);
+        
+        for(int i = 0; i < 10; i++)
+        {
+            if(numero == numeros[i])
+            {
+                printf("Achei o Número %i na posicao %i\n", numero, i);
+                break;
+            }
+        
+        }
+    
+    return 0;
+}
