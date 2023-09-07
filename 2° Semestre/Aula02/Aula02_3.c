eaea921c320c3c85eb1ba44f2c4565f20faa2c58
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2023-09-06 21:40:48 -0300

    Create Aula02_3.c

---
+++ 2° Semestre/Aula02/Aula02_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+   int a,b,*c;
+    a = 0;
+    b = 3;
+    c = &a;
+    *c = *c + 1;
+    c = &b;
+    b = b + 4;
+    
+    printf("%d %d %d", a, b,*c);
+    getchar();
+    
+    
+    
+    return 0;
+}
