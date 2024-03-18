a902f876d48a0deee6175adc0c0ed1ef9f4be6c2
Gustavo Martins <118000156+G2Martins@users.noreply.github.com>
2024-03-17 22:19:11 -0300

    Add files via upload

---
+++ 3° Semestre/Udemy/Aula_10.m
@@ -0,0 +1,17 @@
+%% Mínimo Multíplo Comum
+% Retorna o menor valor que seja múltiplo dos dois argumentos de entrada
+
+% Múltiplos de 3 e 4 são: 12, 24, 36, 48...
+
+lcm(3,4) % Exibe -> 12
+
+% Múltiplos de 5 e 4 são: 20, 40, 60, 80...
+
+lcm(5,4) % Exibe -> 20
+
+%% Máximo Divisor Comum
+% Retorna o máximo divisor entre os dois argumentos de entrada
+
+% Os divisores comuns de 12 e 18 são: 1, 2, 3 e 6.
+
+gcd(12, 18) % Exibe -> 6
---
+++ 3° Semestre/Udemy/Aula_12.m
@@ -0,0 +1,16 @@
+%% Conversões
+
+% Conversão de radianos para graus
+(0.5 *pi) * (180/pi) % Exibe -> 90°
+(1 *pi) * (180/pi) % Exibe -> 180°
+
+rad2deg(0.5*pi) % Meio Pi é 90°
+rad2deg(1*pi) % 180°
+
+% Conversão de graus para radianos
+(90) * (pi/180) % Exibe o valor de 90° em radianos
+(180) * (pi/180) % Exibe o valor de 180° em radianos
+
+deg2rad(45)
+deg2rad(90)
+deg2rad(180)
