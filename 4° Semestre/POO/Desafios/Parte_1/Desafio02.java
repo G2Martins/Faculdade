435f8760fb43598ec5a644a7eef5931e7338a7de
G2Martins <gustavogripaldi@outlook.com>
2024-08-29 15:59:04 -0300

    add: Desafio02 Java files

--- 4° Semestre/POO/Desafios/Parte_1/Desafio02.java
+++ 4° Semestre/POO/Desafios/Parte_1/Desafio02.java
@@ -1,5 +1,5 @@
-/*Receba do usuário via console uma sequência de números inteiros até que Use o comando while. 
-5 números ímpares sejam lidos. 
+/*
+Receba do usuário via console uma sequência de números inteiros até que 5 números ímpares sejam lidos. 
 Imprima o primeiro número ímpar lido. 
 Imprima o último número ímpar lido. 
 Imprima quantas vezes o laço precisou ser repetido. 
@@ -8,8 +8,27 @@ Crie duas variáveis auxiliares para armazenar o primeiro e o último valor ímp
 import java.util.Scanner;
 
 public class Desafio02 {
-    public static void main (){
-
-    }
     
+    public static void main(String[] args)
+    {
+        // Lendo os dados de Entrada:
+        Scanner scanner = new Scanner(System.in);
+        int contadorImpares = 0;
+        int contadorLaco = 0;
+
+        while (contadorImpares < 5 ) 
+        {
+            System.out.print("Digite um número inteiro: ");
+            int numero = scanner.nextInt();
+            contadorLaco++;
+
+            if (numero % 2 == 1) {
+                contadorImpares++;
+            }   
+        }
+        
+        
+        System.out.println("O laço foi repetido " + contadorLaco);
+        scanner.close();
+    }
 }
