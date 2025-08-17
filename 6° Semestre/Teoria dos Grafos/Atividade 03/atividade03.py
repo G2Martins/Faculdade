370b7f8e7f6555360977b6bb1315b7723e9bcdc5
G2Martins <gustavogripaldi@outlook.com>
2025-08-17 16:54:59 -0300

    add: grafos Atividade03 files

---
+++ 6° Semestre/Teoria dos Grafos/Atividade 03/atividade03.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import numpy as np
+
+# 1. Crie um grafo não orientado
+G = nx.Graph()
+
+# 2. Defina os vértices e as arestas
+num_vertices = 6
+arestas = [(0, 1), (0, 2), (0, 5), (1, 0), (1, 2), (1, 3), (1, 5), (2, 0), (2, 1), (2, 4), (3, 1), (3, 3), (3, 4), (3, 5), (4, 2), (4, 3), (4, 5), (5, 0), (5, 1), (5, 3), (5, 4)]
+
+# Adicione os nós (vértices) e as arestas ao grafo
+G.add_nodes_from(range(num_vertices))
+G.add_edges_from(arestas)
+
+# 3. Gere a matriz de adjacência
+# A função do networkx retorna uma matriz em formato esparso (scipy),
+# então convertemos para um array numpy denso para facilitar a visualização.
+matriz_adj = nx.to_numpy_array(G, nodelist=sorted(G.nodes()))
+
+# 4. Imprima a matriz
+print("Vértices:", sorted(G.nodes()))
+print("Matriz de Adjacência:")
+print(matriz_adj)
